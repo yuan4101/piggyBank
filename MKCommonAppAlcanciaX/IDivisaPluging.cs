436d42b547f27cc05d6b1ecc7d600149bd3d680e
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2022-07-27 13:48:22 -0500

    base of plugins implemented

---
+++ MKCommonAppAlcanciaX/IDivisaPluging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKCommonAppAlcanciaX
+{
+    public interface IDivisaPluging
+    {
+        int CalcularSaldoDivisaMonedas(int prmSaldoMonedas);
+        int CalcularSaldoDivisaBilletes(int prmSaldoBilletes);
+        int CalcularSaldoTotal(int prmSaldoTotal);
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class CIngoPlugIn : System.Attribute
+    {
+        private string atrDivisa;
+        private int atrTRM;
+
+        public string darDivisa() 
+        {
+            return atrDivisa;
+        }
+        public int darTRM() 
+        {
+            return atrTRM;
+        }
+    }
+}
---
+++ pluginsDivisaPEN/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MKCommonAppAlcanciaX;
+
+namespace pluginsDivisaPEN
+{
+    public class Class1 : IDivisaPluging
+    {
+        public int CalcularSaldoDivisaBilletes(int prmSaldoBilletes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalcularSaldoDivisaMonedas(int prmSaldoMonedas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalcularSaldoTotal(int prmSaldoTotal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
